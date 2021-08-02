7fa8d1a3719856ad339508392f5b90c50a42cd8b
ImGabe <gabrielpmonte@hotmail.com>
2021-08-02 17:26:19 -0300

    duration optional and rework all files

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const urlInput = document.querySelector("#input-url");
 const numberInput = document.querySelector("#input-number");
+const checkbox = document.querySelector("#checkbox");
+const button = document.querySelector("#button");
 const error = document.querySelector("#error");
 const menu = document.querySelector("#menu");
-const btn = document.querySelector("#btn");
 
 const urlRequest = "https://ec2-18-222-205-41.us-east-2.compute.amazonaws.com/";
 
@@ -11,7 +12,11 @@ if (window.location.hash !== "") {
   window.location.replace(urlRequest + hash);
 }
 
-const appendErro = (errors) => {
+/**
+ * append error messages in `div#error`
+ * @param {string[]} errors array with error messages
+ */
+const appendErrors = (errors) => {
   error.innerHTML = null;
 
   errors.map((errorMessage) => {
@@ -23,20 +28,35 @@ const appendErro = (errors) => {
   });
 };
 
+/**
+ * create `div.item` and append in `div#menu`
+ * @param {object} shortenerURL json with `id` and duration `field`
+ */
 const appendItem = ({ id, duration }) => {
   const item = document.createElement("div");
   item.classList.add("item");
 
+  // create anchor element set href to add #id in current url
   const anchor = document.createElement("a");
   anchor.href = `${window.location.href}#${id}`;
-  anchor.innerText = id;
   anchor.target = "_blank";
 
-  const spanDuration = document.createElement("span");
-  const date = new Date();
-  date.setSeconds(date.getSeconds() + duration);
-  spanDuration.innerText = date.toLocaleTimeString();
+  // create code element to standard size of ids
+  const code = document.createElement("code");
+  code.innerText = id;
+  anchor.appendChild(code);
 
+  // create span element with duration if input is enable else "Forever"
+  const spanDuration = document.createElement("span");
+  if (!numberInput.disabled) {
+    const date = new Date();
+    date.setSeconds(date.getSeconds() + duration);
+    spanDuration.innerText = date.toLocaleTimeString();
+  } else {
+    spanDuration.innerText = "Forever";
+  }
+
+  // create span element and add click event to copy the shortener url
   const spanCopy = document.createElement("span");
   spanCopy.classList.add("copy");
   spanCopy.innerText = "Copy";
@@ -45,32 +65,50 @@ const appendItem = ({ id, duration }) => {
     await navigator.clipboard.writeText(url).catch(console.error);
   };
 
+  // append elements in item element
   item.appendChild(anchor);
   item.appendChild(spanDuration);
   item.appendChild(spanCopy);
+  // append item in menu element
   menu.appendChild(item);
 };
 
+/**
+ * Check types errors in `url` and `duration`
+ * @param {string} url url shortener
+ * @param {number} duration url duration
+ * @returns errors array length
+ */
 const filterError = (url, duration) => {
   const errors = [];
 
+  // check if have content in url
   if (!url) errors.push("The URL field is empty.");
-  duration ?? errors.push("The duration field is empty.");
-
-  if (Math.sign(duration) !== 1)
+  // check if input is enable and content is a positive integer
+  if (!numberInput.disabled && Math.sign(duration) !== 1)
     errors.push("Use only integers and positives.");
 
-  appendErro(errors);
+  appendErrors(errors);
 
   return errors.length;
 };
 
-btn.addEventListener("click", async () => {
-  const url = urlInput.value;
-  const duration = Math.trunc(numberInput.value);
+// disable number input by default
+numberInput.disabled = true;
 
+checkbox.addEventListener(
+  "change",
+  () => (numberInput.disabled = !numberInput.disabled)
+);
+
+button.addEventListener("click", async () => {
+  const url = urlInput.value;
+  const duration = Math.trunc(numberInput.value) || 0;
+
+  // if have any error stop here
   if (filterError(url, duration)) return;
 
+  // POST in `urlRequest` to save in database
   await fetch(urlRequest, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -79,5 +117,5 @@ btn.addEventListener("click", async () => {
   })
     .then((res) => res.json())
     .then(appendItem)
-    .catch(console.error);
+    .catch(() => appendErrors(['Not is possible to connect with server']));
 });
